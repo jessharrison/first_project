ffb0bc94adff63f0f3b28f5fc0ae0684173478c5
Jessica Forsyth <jessicalforsyth@gmail.com>
2018-12-15 11:41:29 -0800

    Initial Commit

---
+++ first_project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  first_project
+//
+//  Created by Jessica Forsyth on 12/15/18.
+//  Copyright © 2018 Jessica Forsyth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
